f5e8f25b274e4417128df9b521b78406db3b72c3
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-04-14 13:46:22 -0300

    Use useState to create the state in the SongList.

--- hooksApp/hooksapp/src/components/SongList.jsx
+++ hooksApp/hooksapp/src/components/SongList.jsx
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const SongList = () => {
+  const [songs, setSongs] = useState([
+    { title: 'musica1', id:0},
+    { title: 'musica2', id:1},
+    { title: 'musica3', id:2},
+  ]);
+  const addSong = () => {
+    setSongs([...songs, {title:'new song', id: songs.length} ])
+  }
   return ( 
     <div className='song-list'>
       <ul>
-        <li>This wild darkness</li>
-        <li>Memory</li>
+        {songs.map((song) => <li key={song.id}>{song.title}</li>)}
       </ul>
+      <button onClick={addSong}>Add a song</button>
     </div>
-   );
+  );
 }
- 
+
 export default SongList;
